52ce59ebc52701ce29b5a9d058ab93a34e158143
Carlos <toxz@foxmail.com>
2015-01-28 21:59:45 +0800

    TileListView Function

--- Library/src/main/java/com/unique/yyz/tilelistview/library/TileListView.java
+++ Library/src/main/java/com/unique/yyz/tilelistview/library/TileListView.java
@@ -2,6 +2,7 @@ package com.unique.yyz.tilelistview.library;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.database.DataSetObserver;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -9,11 +10,13 @@ import android.graphics.drawable.Drawable;
 import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 
 
 /**
- * TODO: document your custom view class.
+ * A ListView to display all its items.
  */
 public class TileListView extends ListView {
 
@@ -29,5 +32,34 @@ public class TileListView extends ListView {
         super(context, attrs, defStyle);
     }
 
+    @Override
+    public void setAdapter(ListAdapter adapter) {
+        super.setAdapter(adapter);
 
+        adapter.registerDataSetObserver(new DataSetObserver() {
+            @Override
+            public void onChanged() {
+                fitHeight();
+            }
+        });
+    }
+
+    /**
+     * adjust height of the view to contain all the items
+     */
+    private void fitHeight() {
+        ListAdapter adapter = getAdapter();
+
+        int heightSum = 0;
+        for (int i = 0; i < adapter.getCount(); i++) {
+            View listItem = adapter.getView(i, null, this);
+            listItem.measure(0, 0);
+            heightSum += listItem.getMeasuredHeight();
+        }
+
+        ViewGroup.LayoutParams params = this.getLayoutParams();
+        int dividerHeights = getDividerHeight() * (adapter.getCount() == 0 ? 0 : adapter.getCount() - 1);
+        params.height = heightSum + dividerHeights;
+        setLayoutParams(params);
+    }
 }
